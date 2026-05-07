c38313ada4172175888d580f26b94335834bfcf7
beatriziw <beatriz.ssilva@sptech.school>
2026-05-07 08:27:35 -0300

    ajustando api

--- web-data-viz-main/src/controllers/usuarioController.js
+++ web-data-viz-main/src/controllers/usuarioController.js
@@ -54,23 +54,17 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    // var email = req.body.emailServer;
+    // var senha = req.body.senhaServer;
+    // var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -86,6 +80,15 @@ function cadastrar(req, res) {
                 }
             );
     }
+
+    // else if (email == undefined) {
+    //     res.status(400).send("Seu email está undefined!");
+    // } else if (senha == undefined) {
+    //     res.status(400).send("Sua senha está undefined!");
+    // } else if (fkEmpresa == undefined) {
+    //     res.status(400).send("Sua empresa a vincular está undefined!");
+    // } 
+
 }
 
 module.exports = {
